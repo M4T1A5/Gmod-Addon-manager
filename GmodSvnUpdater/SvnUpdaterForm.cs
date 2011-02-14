16cdf9efc9608cf1faa51c7cdd64ed67d59a557c
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2011-02-14 16:46:45 +0200

    Added complete functionality to Add and Remove buttons

--- GmodSvnUpdater/SvnUpdaterForm.cs
+++ GmodSvnUpdater/SvnUpdaterForm.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GmodSvnUpdater.Properties;
+using Microsoft.VisualBasic;
 using SharpSvn;
 
 namespace GmodSvnUpdater
@@ -70,5 +71,73 @@ namespace GmodSvnUpdater
                 MessageBox.Show(Resources.updateAddonDirErrorMessage, Resources.updateErrorHeader);
             }
         }
+        
+        void AddButtonClick(object sender, EventArgs e)
+        {
+        	if (Properties.Settings.Default.AddonDir == String.Empty) 
+        	{
+        		MessageBox.Show(Resources.updateAddonDirErrorMessage, Resources.updateErrorHeader);
+        		return;
+        	}
+			var answer = Microsoft.VisualBasic.Interaction.InputBox("Please give the url to the repository",
+        	                                                        "Add...", "", 100, 100);
+        	if (answer != String.Empty)
+        	{
+        		Uri url = new Uri(answer);
+	        	using(SvnClient svnClient = new SvnClient())
+	        	{
+	        		var dir = Microsoft.VisualBasic.Interaction.InputBox("Please give name of the folder where to save\nTHIS MUST BE SET IF NOT TOLD OTHERWISE\nUsually the name of the mod", "Folder name...", "", 100, 100);
+	        		svnClient.CheckOut(url, Properties.Settings.Default.AddonDir + "\\" + dir);
+	        	}
+	        	InitializeSvnStuff();
+        	}
+        }
+        
+        void RemoveButtonClick(object sender, EventArgs e)
+        {
+        	DialogResult dlgResult = MessageBox.Show("Do you really want to remove "+ listAddonsList.SelectedItems[0].Text + "?", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (dlgResult == DialogResult.Yes)
+			{
+				DeleteSubFolders(Properties.Settings.Default.AddonDir + "\\" + listAddonsList.SelectedItems[0].Text, ".svn", true);
+				Directory.Delete(Properties.Settings.Default.AddonDir + "\\" + listAddonsList.SelectedItems[0].Text, true);
+			}
+			InitializeSvnStuff();
+        }
+		public static void DeleteSubFolders(string folderPath, string wildcardPattern, bool top)
+		{
+		String[] list;
+		
+		//If we are at the top level, get all directory names that match the pattern
+		if (top)
+		list = Directory.GetDirectories(folderPath, wildcardPattern, SearchOption.AllDirectories);
+		else //Get directories and files for matching sub directories
+		list = Directory.GetFileSystemEntries(folderPath, wildcardPattern);
+		
+		foreach (string item in list)
+		{
+		//Sub directories
+		if (Directory.Exists(item))
+		{
+		//Match all sub directories
+		DeleteSubFolders(item, "*", false);
+		}
+		else // Files in directory
+		{
+		//Get the attribute for the file
+		FileAttributes fileAtts = File.GetAttributes(item);
+		//If it is read only make it writable
+		if ((fileAtts & FileAttributes.ReadOnly) != 0)
+		{
+		File.SetAttributes(item, fileAtts & ~FileAttributes.ReadOnly);
+		}
+		File.Delete(item);
+		}
+		}
+		//Delete the matching folder that we are in
+		if (top == false)
+		{
+		Directory.Delete(folderPath);
+		}
+		}
     }
 }
